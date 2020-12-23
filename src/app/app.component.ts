e96976506c3e0129f9a98b0e80919a518b4cb29e
Mattie Schraeder <mattie@mattie.lgbt>
2020-12-23 12:06:10 -0600

    Working menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { Observable } from 'rxjs'
+import { map, shareReplay } from 'rxjs/operators'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'mattie'
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    )
+
+  isTablet$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Tablet)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    )
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public router: Router
+  ) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { GraphQLModule } from './graphql.module'
 import { HttpClientModule } from '@angular/common/http'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatMenuModule } from '@angular/material/menu'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { MatTooltipModule } from '@angular/material/tooltip'
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +21,12 @@ import { HttpClientModule } from '@angular/common/http'
     BrowserAnimationsModule,
     GraphQLModule,
     HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatTooltipModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
