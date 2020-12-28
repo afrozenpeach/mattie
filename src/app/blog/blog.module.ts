cdd9a824b281a5410ed777902a4ad957834f1fdc
Mattie Schraeder <mattie@mattie.lgbt>
2020-12-28 10:24:19 -0600

    Add times to the archives page

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common'
 import { RouterModule, Routes } from '@angular/router'
 import { PostComponent } from './post/post.component'
 import { ArchivesComponent } from './archives/archives.component'
+import { MomentModule } from 'ngx-moment'
+import { MomentTimezoneModule } from '../shared/moment-timezone/moment-timezone.module'
 
 const blogRoutes: Routes = [
   { path: '', component: ArchivesComponent, pathMatch: 'full' },
@@ -11,6 +13,11 @@ const blogRoutes: Routes = [
 
 @NgModule({
   declarations: [ArchivesComponent, PostComponent],
-  imports: [CommonModule, RouterModule.forChild(blogRoutes)],
+  imports: [
+    CommonModule,
+    MomentModule,
+    MomentTimezoneModule,
+    RouterModule.forChild(blogRoutes),
+  ],
 })
 export class BlogModule {}
